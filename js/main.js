c11c474647b351a60b9a1109788d8aa0edb1aa44
Elizabeth Messick <Elizabeth@MacBook-Pro-4.local>
2018-12-08 13:40:28 -0800

    create portfolio

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("myTopnav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+function hamburgerMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
