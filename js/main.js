47098c0e62a0010460e6c55df9080f01fde7d876
Elizabeth Messick <Elizabeth@macbook-pro-4.attlocal.net>
2018-12-10 22:43:43 -0800

    fix contact footer

--- js/main.js
+++ js/main.js
@@ -1,41 +1,13 @@
-// window.onscroll = function () { myFunction() };
 
-// var navbar = document.getElementById("myTopnav");
-// var sticky = navbar.offsetTop;
-
-// function myFunction() {
-//     if (window.pageYOffset >= sticky) {
-//         navbar.classList.add("sticky")
-//     } else {
-//         navbar.classList.remove("sticky");
-//     }
-// }
-
-// function hamburgerMenu() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//         x.className += " responsive";
-//     } else {
-//         x.className = "topnav";
-//     }
-// }
-
-// HEADER ANIMATION
 $(".fade").hide(0).fadeIn(850)
 
-// BURGER MENU
 document.addEventListener('DOMContentLoaded', () => {
-    // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
-        // Add a click event on each of them
         $navbarBurgers.forEach(el => {
             el.addEventListener('click', () => {
-                // Get the target from the "data-target" attribute
                 const target = el.dataset.target;
                 const $target = document.getElementById(target);
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
                 el.classList.toggle('is-active');
                 $target.classList.toggle('is-active');
             });
@@ -43,12 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-// DOM LOAD ANIMATE ARROW
-// $(document).ready(function() {
-//   $("#element-to-animate").addClass("animation"); 
-//   });
-
-// SCROLL REVEAL
 window.sr = ScrollReveal({ reset: false });
 sr.reveal(
     '.reveal', {
@@ -60,26 +26,17 @@ sr.reveal(
         mobile: true,
         viewFactor: 0.4
     });
-// sr.reveal('.bar');
 
-// SMOOTH SCROLL TO SECTION
 $(document).ready(function () {
-    // Add smooth scrolling to all links
     $("a").on('click', function (event) {
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            // Prevent default anchor click behavior
             event.preventDefault();
-            // Store hash
             var hash = this.hash;
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 900, function () {
-                // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
             });
-        } // End if
+        }
     });
 });
